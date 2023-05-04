58408f260f6c557e49429f064f89090bb68e06a4
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-04 18:48:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ function getValue()
 let a = document.getElementById("name").value;
 let b = document.getElementById("year").value;
 let show = "https://localost:8080/?" ;
-if(a == "")
+if(a == "" && b != "")
 {
     document.getElementById("url").innerHTML = show +"year=" + b;
 }
-else if(b == "")
+else if(b == "" && a !="")
 {
     document.getElementById("url").innerHTML = show + "name=" + a;
 }
-else{
+else if(a != "" && b != ""){
     document.getElementById("url").innerHTML = show + "name=" + a +"&"+"year=" + b;
 }
+else{
+    document.getElementById("url").innerHTML = show;
+}
+
 }
