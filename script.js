f2854143bc475e166ef73635e8102c5c9cf4c10f
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-04 18:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,19 @@ function getValue()
 {
 let a = document.getElementById("name").value;
 let b = document.getElementById("year").value;
-let show = "https://localost:8080/?" ;
+let show = "https://localost:8080/" ;
 if(a == "" && b != "")
 {
-    document.getElementById("url").innerHTML = show +"year=" + b;
+    document.getElementById("url").innerHTML = show +"?year=" + b;
 }
 else if(b == "" && a !="")
 {
-    document.getElementById("url").innerHTML = show + "name=" + a;
+    document.getElementById("url").innerHTML = show + "?name=" + a;
 }
 else if(a != "" && b != ""){
-    document.getElementById("url").innerHTML = show + "name=" + a +"&"+"year=" + b;
+    document.getElementById("url").innerHTML = show + "?name=" + a +"&"+"year=" + b;
 }
 else{
     document.getElementById("url").innerHTML = show;
 }
-
 }
