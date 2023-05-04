7a9f2222cf4853181b91735158e1d333e8ff875d
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-04 18:38:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// your code here
+function getValue()
+{
+let a = document.getElementById("name").value;
+let b = document.getElementById("year").value;
+let show = "https://localost:8080/?" ;
+if(a == "")
+{
+    document.getElementById("url").innerHTML = show +"year=" + b;
+}
+else if(b == "")
+{
+    document.getElementById("url").innerHTML = show + "name=" + a;
+}
+else{
+    document.getElementById("url").innerHTML = show + "name=" + a +"&"+"year=" + b;
+}
+}
